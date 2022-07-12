2f4eea5f7cb7afb8b9868b6a6e825383e04926c6
SSSWeb-1 <opentech4u@gmail.com>
2022-07-12 10:59:38 +0530

    adding fabicon

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="icon" href="<?php echo base_url("/confed.jpg"); ?>">
 <title>Confed Efile management</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
