f84dad760af6091372453cba3a2aa391c714c2c6
SSSWeb-1 <opentech4u@gmail.com>
2022-08-08 17:53:09 +0530

    change

--- application/views/registration.php
+++ application/views/registration.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Dashboard</title>
+<title>Registration</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">-->
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
